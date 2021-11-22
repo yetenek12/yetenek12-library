785d288035cb01feaee84a0756e264db707357a7
Kagan Inan <contact@kaaninan.net>
2021-11-22 23:38:20 +0300

    update

--- examples/basic_reading/basic_reading.ino
+++ examples/basic_reading/basic_reading.ino
@@ -3,14 +3,9 @@
 
 void setup(){
 	Serial.begin(115200);
-	yetenek.init(Sensors::commType::i2c);
+	yetenek.init();
 }
 
 
 void loop(){
-	int dis = yetenek.getDistance();
-	int freq = yetenek.getSoundFreq();
-	int amp = yetenek.getSoundAmplitude();
-	Serial.printf("Distance: %d\tSound Frequency: %ld\tSound Amplitude: %d", dis, freq, amp);
-	delay(1000);
 }
